3388bf1d7c0a5135ef2d9c187a352a77ca97473a
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-10-28 15:19:27 +0400

    feature/dinamic routt

---
+++ src/app/(views)/sevice/[id]/page.tsx
@@ -0,0 +1,21 @@
+const CustomPost = ({
+  params,
+}: {
+  params: {
+    id: string;
+  };
+}) => {
+  return (
+    <div className='w-full flex flex-col justify-center items-center font-[Raleway]'>
+      <div className='max-w-[1440px] w-full '>
+        <div className='w-full mt-[148px] flex justify-center'>
+          <h1 className='max-w-[764px] w-full font-bold text-[73px] leading-[74px] text-center text-[#252432]'>
+            your service number is : {params.id}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomPost;
