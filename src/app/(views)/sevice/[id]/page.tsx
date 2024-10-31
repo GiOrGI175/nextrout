bdebee75337d50ee88446f76620cbfb1c0bc1ce0
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2024-10-31 16:11:43 +0400

    fix

--- src/app/(views)/sevice/[id]/page.tsx
+++ src/app/(views)/sevice/[id]/page.tsx
@@ -1,16 +1,16 @@
-const CustomPost = ({
-  params,
-}: {
+type CustomPostProps = {
   params: {
     id: string;
   };
-}) => {
+};
+
+const CustomPost: React.FC<CustomPostProps> = ({ params }) => {
   return (
     <div className='w-full flex flex-col justify-center items-center font-[Raleway]'>
       <div className='max-w-[1440px] w-full '>
-        <div className='w-full mt-[148px] flex justify-center'>
-          <h1 className='max-w-[764px] w-full font-bold text-[73px] leading-[74px] text-center text-[#252432]'>
-            your service number is : {params.id}
+        <div className='w-full mt-[148px] flex justify-center '>
+          <h1 className=' max-w-[764px] w-full font-bold text-[73px] leading-[74px] text-center text-[#252432] '>
+            your service number is: {params.id}
           </h1>
         </div>
       </div>
